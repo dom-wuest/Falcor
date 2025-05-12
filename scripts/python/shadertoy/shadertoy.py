a04d323253189a799d71b85e6dca500ab88361ae
Dominik Wüst <dominik.wuest@student.kit.edu>
2025-05-12 11:46:03 +0200

    Added Raymarching-Primitives Shadertoy. added screenshot capability.

--- scripts/python/shadertoy/shadertoy.py
+++ scripts/python/shadertoy/shadertoy.py
@@ -1,26 +1,42 @@
 import falcor
 from pathlib import Path
 import time
+from PIL import Image
 
 DIR = Path(__file__).parent
 WIDTH = 800
-HEIGHT = 600
+HEIGHT = 450
+CREATE_WINDOW = True
+SHOW_UI = True
 
-testbed = falcor.Testbed(create_window=True, width=WIDTH, height=HEIGHT)
-testbed.show_ui = True
+# === Screenshot Utility ===
+def screenshot_falcor(filename):
+    # write the output to a file using falcor api
+    testbed.capture_output(DIR.parent.parent.parent / "captures" / filename, 0)
+
+def screenshot_numpy(filename):
+    # use the output resource and convert to numpy array
+    output_resource = graph.get_output("Shadertoy.output")
+
+    img = output_resource.to_numpy().reshape((HEIGHT, WIDTH, 4))
+    img_rgba8 = img[:,:, [2,1,0,3]] # reorder channels to RGBA
+
+    Image.fromarray(img_rgba8).save(DIR.parent.parent.parent / "captures" / filename)
+
+# === Initialize the Testbed ===
+testbed = falcor.Testbed(create_window=CREATE_WINDOW, width=WIDTH, height=HEIGHT)
+testbed.show_ui = SHOW_UI
 device = testbed.device
 
 # === Create a Render Graph ===
 graph = testbed.create_render_graph("ShadertoyRenderGraph")
 
-#params = {"shaderPath": shader_path}
-params = {}
+params = {"shaderPath": DIR / "primitives.ps.slang"}
 shadertoy = graph.create_pass("Shadertoy", "Shadertoy", params)
 
 graph.mark_output("Shadertoy.output")
 
 testbed.render_graph = graph
-
 
 print(shadertoy.properties)
 
@@ -37,12 +53,18 @@ while not testbed.should_close:
     elapsed_time = current_time - start_time
     frame_idx += 1
 
+    # === Update the Uniform Variables ===
     shadertoy.shaderInputs.iResolution = [WIDTH, HEIGHT, 0]
     shadertoy.shaderInputs.iTime = elapsed_time
     shadertoy.shaderInputs.iTimeDelta = delta_time
     shadertoy.shaderInputs.iFrameRate = 1.0 / delta_time
     shadertoy.shaderInputs.iFrame = frame_idx
 
-    testbed.frame()
+    testbed.frame() # render the frame to the framebuffer
+
+    if frame_idx == 10000: # take a screenshot at 10000 frames
+        screenshot_falcor(f"ShadertoyOutput{frame_idx}_falcor.png")
+        screenshot_numpy(f"ShadertoyOutput{frame_idx}_numpy.png")
+
 
 print(shadertoy.properties)
